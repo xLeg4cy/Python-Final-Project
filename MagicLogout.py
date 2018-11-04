a75c27d6af3c492da754a3079dc2a6d1cf20c982
xLeg4cy <43527470+xLeg4cy@users.noreply.github.com>
2018-11-04 09:06:49 -0500

    Update MagicLogout.py

--- MagicLogout.py
+++ MagicLogout.py
@@ -1,4 +1,12 @@
+import os
+import json
 
+#Server Configuration
+with open ("config.json") as json_file:
+  json_date = json.loan(json_file)
+  server = json_data["server"]["host"]
+  
+  def magiclogout():
+    while True:
+      
 
-
-import OS
