8e08b308515d36b1b47e423b9523d579ff187bab
xLeg4cy <43527470+xLeg4cy@users.noreply.github.com>
2018-12-05 03:47:52 -0500

    Update MagicLogout.py

--- MagicLogout.py
+++ MagicLogout.py
@@ -3,7 +3,7 @@ import json
 
 #Server Configuration
 with open ("config.json") as json_file:
-  json_date = json.loan(json_file)
+  json_date = json.load(json_file)
   server = json_data["server"]["host"]
   
   def magiclogout():
@@ -12,4 +12,12 @@ with open ("config.json") as json_file:
       print("")
       os.system("C:\\Windows\System32\\  "+username+" /server:"+server)
                 print("")
-      session = input("session name or id to logoff:>")
+      session = (input("session name or id to logoff:>"))
+      os.system("C:\\Windows\\System32\\logoff.exe "+ session+ " /server:"+server)
+      os.system("cls")
+      print(username+ "logoff")
+print(""" 
+	        Qrdp disconnect session by:xLeg4cy and Bursoft
+			                                        """ )
+                
+magiclogout()
